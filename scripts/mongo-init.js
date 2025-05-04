8e64217ec87a1043a758ca973597d59f80d02a94
Siddharth Suresh <suresh.siddharth@gmail.com>
2025-05-04 15:07:34 +0000

    Final modifications for deployment

--- scripts/mongo-init.js
+++ scripts/mongo-init.js
@@ -1,7 +1,7 @@
 // Read the application username and password from the secret files mounted by Docker
 // Note: trim() removes potential trailing newlines from the secret files.
-const appUser = cat('/run/secrets/DB_APP_USERNAME').trim();
-const appPassword = cat('/run/secrets/DB_APP_PASSWORD').trim();
+const appUser = fs.readFileSync('/run/secrets/DB_APP_USERNAME', 'utf8').trim();
+const appPassword = fs.readFileSync('/run/secrets/DB_APP_PASSWORD', 'utf8').trim();
 const targetDb = 'wishful'; // The database the application user needs access to
 
 // Switch to the 'admin' database to create the user
